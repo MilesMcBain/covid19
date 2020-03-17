994a6e2f2f6364d21373e048f817a18524e5bc41
Miles McBain <miles.mcbain@gmail.com>
2020-03-18 08:40:52 +1000

    label aus states

--- R/get_doubling_rates_aus.R
+++ R/get_doubling_rates_aus.R
@@ -9,20 +9,26 @@
 ##' @export
 get_doubling_rates_aus <- function(cases_df) {
 
-  state_cases  <- 
+  state_cases <-
     cases_df %>%
-    filter(country_region == "Australia",
-           cases > 0)
+    filter(
+      country_region == "Australia",
+      cases > 0
+    )
 
   time_to_double <-
     state_cases %>%
     group_by(province_state) %>%
-    mutate(lead_date = lead(date),
-           n_double = floor(log(cases, 2) - log(min(cases), 2))) %>%
+    mutate(
+      lead_date = lead(date),
+      n_double = floor(log(cases, 2) - log(min(cases), 2))
+    ) %>%
     ## a = b * 2^n => log(a,2) - log(b,2) = n
     group_by(province_state, n_double) %>%
-    mutate(time_to_double = as.numeric(max(lead_date) - min(date),
-                                       units = "days")) %>%
+    mutate(time_to_double = as.numeric(
+      max(lead_date) - min(date),
+      units = "days"
+    )) %>%
     ## lead_date is the date of change
     ungroup() %>%
     group_by(province_state) %>%
@@ -33,13 +39,13 @@ get_doubling_rates_aus <- function(cases_df) {
       cases = min(cases),
       double_target = min(double_target)
     ) %>%
-      drop_na(time_to_double) %>%
+    drop_na(time_to_double) %>%
     ungroup()
 
-    ## NAs occur in the final rows for each country, where we're still counting
-    ## toward the next double.
+  ## NAs occur in the final rows for each country, where we're still counting
+  ## toward the next double.
 
   time_to_double
-    
+
 
 }
--- R/plot_doubline_rates_aus.R
+++ R/plot_doubline_rates_aus.R
@@ -11,9 +11,11 @@ plot_doubline_rates_aus <- function(doubling_rates_aus) {
 
   p <-
     doubling_rates_aus %>%
-    ggplot(aes(x = double_target,
-               y = jitter(time_to_double),
-               colour = province_state)) +
+    ggplot(aes(
+      x = double_target,
+      y = jitter(time_to_double),
+      colour = province_state
+    )) +
     geom_line() +
     labs(
       title = "Observed days to double number of COVID19 cases",
@@ -22,8 +24,26 @@ plot_doubline_rates_aus <- function(doubling_rates_aus) {
       x = "from number of cases"
     ) +
     scale_x_continuous(trans = "log2", labels = scales::comma) +
-    theme_minimal()
-    
+    theme_minimal() +
+    gghighlight(
+      province_state %in% c(
+        "Australian Capital Territory",
+        "New South Wales",
+        "Queensland",
+        "South Australia",
+        "Tasmania",
+        "Victoria",
+        "Western Australia"
+      ),
+      label_params = list(
+        size = 2,
+        nudge_y = 10,
+        segment.alpha = 0.2
+      ),
+      calculate_per_facet = TRUE,
+      use_group_by = FALSE
+    )
+
   p
 
 }
